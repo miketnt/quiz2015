5480bc8c248d07ccbe3dae1a885c000ec5a57ad6
Miguel Ángel <mitxel@gmail.com>
2015-08-15 19:15:57 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,11 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load); // autoload :quizId
+router.param('quizId', quizController.load); 		// autoload :quizId
+router.param('commentId', commentController.load); 	// autoload :commentId
 
 // Definición de rutas de sesión
-router.get('/login',	sessionController.new);	// formulario login
+router.get('/login',	sessionController.new);		// formulario login
 router.post('/login',	sessionController.create);	// crear sesión
 router.get('/logout',	sessionController.destroy);	// destruir sesión
 
@@ -33,7 +34,11 @@ router.get('/author', function(req, res){
   res.render('author', {errors: []});
 });
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+									sessionController.loginRequired, commentController.publish);
+
 
 module.exports = router;
